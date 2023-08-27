49a964c2d83aa9f4a52f56698b7c248ebe8d1af8
oarina <>
2023-08-27 22:12:36 +0000

    Wire gspread API's and confirm sheet connection

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread # imports intire gspread library
+from google.oauth2.service_account import Credentials # imports credentials class
+
+# constant var that tells computer what we are going to access
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('btc_usd_backtesting')
+
+# setting up to read the tab in the sheet
+historical_data = SHEET.worksheet('historical_data')
+
+data = historical_data.get_all_values()
+
+print(data) #just worked! =) 
