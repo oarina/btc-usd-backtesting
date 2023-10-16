18019444844761b3cb2a8b479669a13413bdf3a5
oarina <>
2023-10-16 11:03:05 +0000

    Daily data bars retrieved as a test

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 import gspread # imports entire gspread library
 from google.oauth2.service_account import Credentials # imports credentials class
-from alpaca.data.historical import CryptoHistoricalDataClient 
+from alpaca.data.historical import CryptoHistoricalDataClient # https://alpaca.markets/docs/python-sdk/market_data.html 
+
+from alpaca.data.requests import CryptoBarsRequest
+from alpaca.data.timeframe import TimeFrame
+from datetime import datetime   
 
 # constant var that tells computer what we are going to access
 SCOPE = [
@@ -20,5 +24,25 @@ historical_data = SHEET.worksheet('historical_data')
 data = historical_data.get_all_values()
 
 print(data) #just worked! =) 
-# Writing this for a commit test
 
+#Testing the Alpaca hsitorical data client 
+
+# --1 USING THE CLIENT
+crypto_client = CryptoHistoricalDataClient() 
+
+client = CryptoHistoricalDataClient()
+
+request_params = CryptoBarsRequest(
+                        symbol_or_symbols=["BTC/USD"],
+                        timeframe=TimeFrame.Day,
+                        start=datetime(2023, 10, 1),
+                        end=datetime(2023, 10, 2)
+                 )
+
+bars = client.get_crypto_bars(request_params)
+
+
+# access bars as list - important to note that you must access by symbol key
+# even for a single symbol request - models are agnostic to number of symbols
+bars["BTC/USD"]
+print(bars)
