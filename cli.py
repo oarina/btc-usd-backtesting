0248c19afc0fae9ac201450f1f7a12f177f3c80b
oarina <>
2023-10-21 19:25:43 +0000

    Add cli.py document for a CLI module

---
+++ cli.py
@@ -0,0 +1,10 @@
+# This module is for CLI related functions
+
+# def display_start_welcome_message()
+
+# def display_trade_entry_example()
+
+# def display_trade_calculation_result()
+
+# def display_end_program_message()
+
