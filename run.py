3dddbc17bd4299bffe0970a65e015f8b5531ab04
oarina <>
2023-10-25 09:03:18 +0000

    Write a new data batch of price data into sheet

--- run.py
+++ run.py
@@ -13,25 +13,8 @@ from alpaca_trade_api.rest import REST
 import alpaca_trade_api as tradeapi
 import traceback
 
-# --------- Alpaca setup
 
-def load_alpaca_credentials(filename="cred2.json"):
-    """Load Alpaca API credentials from a json file"""  
-    with open('creds2.json', 'r') as file:
-        creds2 = json.load(file)
-        # print(creds2) 
-        api_key = creds2['ALPACA_API_KEY']
-        api_secret = creds2['ALPACA_API_SECRET']
-    return api_key, api_secret
-
-# Load the API credentials
-API_KEY, API_SECRET = load_alpaca_credentials() 
-alpaca =  tradeapi.REST(API_KEY, API_SECRET, base_url='https://paper-api.alpaca.markets')
-account = alpaca.get_account()
-# print(account) 
-
-
-# --------- Gspread setup
+# ------------------------------------------------ Gspread API setup
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -48,73 +31,30 @@ SHEET = GSPREAD_CLIENT.open('btc_usd_backtesting')
 historical_data = SHEET.worksheet('historical_data')
 data = historical_data.get_all_values()
 
-# ------------------------------------------------- Testing the Alpaca hsitorical data client
+# ------------------------------------------------- Alpaca hsitorical data client
+# Reference: https://alpaca.markets/docs/market-data/getting-started/ 
 
 client = CryptoHistoricalDataClient()
 request_params = CryptoBarsRequest(
         symbol_or_symbols=["BTC/USD"],
         timeframe=TimeFrame(15, TimeFrameUnit.Minute),
-        start=datetime(2021, 1, 1),
-        end=datetime(2023, 10, 19)
+        start=datetime(2023, 10, 19),
+        end=datetime(2023, 10, 24)
         )
 btc_bars = client.get_crypto_bars(request_params)
 btc_bars.df
 print(btc_bars)
 
 
-
-# --1 USING THE CLIENT
-#crypto_client = CryptoHistoricalDataClient()
-# Reference: https://alpaca.markets/docs/market-data/getting-started/ 
-
-
-
-'''
--------------------
-try:
-    client = CryptoHistoricalDataClient()  
-    request_params = CryptoBarsRequest(
-                        symbol_or_symbols=["BTC/USD"],
-                        timeframe=TimeFrame.Day,
-                        start="2022-09-01",
-                        end="202-09-02"
-                        )
-
-
-    bars = client.get_crypto_bars(request_params)
-    print(bars) 
-    # prints what's in the client call
-    # print(data)  prints whats in the google sheet
-except Exception as e:
-    print(e)
-    print(traceback.format_exc()) 
-    print(f"An error occurred during Alpaca client call for data")
-    exit()
-----------------------
-
-  timeframe=TimeFrame(15, TimeFrameUnit.Minute),
-        start=datetime(2021, 1, 1),
-        end=datetime(2021, 1, 2) 
-        so I am not able to get any lower than 2021 for minutes. I will try daily
-
-
-    request_params = CryptoBarsRequest(
-        symbol_or_symbols=["BTC/USD"],
-        timeframe=TimeFrame(15, TimeFrameUnit.Minute),
-        start=datetime(2020, 1, 1, 0, 0, 0),
-        end=datetime(2020, 1, 1, 6, 0, 0)
-    )
-
-'''
-
 # ------------------------------------------------ TESTING WRITING ONTO GSHEET
 
 # historical_datahe workseet of the test columns and a row. https://docs.gspread.org/en/latest/user-guide.html#clear-a-worksheet
 # worksheet.clear()
 # so the command worked and it did delete the test data, but in the terminal it says:NameError: name 'worksheet' is not defined
-"""
 
-# Extract the relevant data from Alpaca list of lists
+
+'''
+# Extract the relevant data from Alpaca list of lists and then insert it into the google sheet. 
 data_to_insert = []
 for data_point in bars["BTC/USD"]:
   
@@ -128,14 +68,39 @@ for data_point in bars["BTC/USD"]:
 
     row = [timestamp_str, low_value, high_value]
     data_to_insert.append(row)
-'''
+    
+
 # Heading for the first row
 headers = ["Timestamp", "Low", "High"]
 historical_data.insert_row(headers, index=1, value_input_option='RAW')
-'''
 
 # Insert the data into the worksheet starting from row 2
 historical_data.insert_rows(data_to_insert, row=2, value_input_option='RAW')
 # OK I really hope it will incert into the 2nd row and not touch the existing info. 
+'''
 
-"""
+
+last_entry = historical_data.row_values(len(historical_data.col_values(1)))
+print(last_entry) # ['2023-10-19 00:00:00', '28300.878', '28345.3785']
+
+data_to_insert = []
+for data_point in btc_bars["BTC/USD"]:
+    timestamp_str = data_point.timestamp.strftime('%Y-%m-%d %H:%M:%S')
+    low_value = data_point.low
+    high_value = data_point.high
+
+    row = [timestamp_str, low_value, high_value]
+    data_to_insert.append(row)
+
+ # Insert new rows after the last filled row.  - https://stackoverflow.com/questions/40781295/how-to-find-the-first-empty-row-of-a-google-spread-sheet-using-python-gspread
+insert_into_last_empty_row = len(historical_data.col_values(1)) + 1  # counts through the rows till the last filled one and +1 find the next empty row
+# it printed 2023-10-19 00:00:00 twice  and since I have asked to plus 1 here - len(historical_data.col_values(1)) + 1 it printed the 2023-10-19 00:00:00 on the next line. 
+# I am removing the +1 so that It can print over the same date. 
+# Furthermore - I will be calling only for a day before because it fills till 00:00:00 for the day
+
+historical_data.insert_rows(data_to_insert, row=insert_into_last_empty_row, value_input_option='RAW')
+
+
+
+
+
